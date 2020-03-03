1ba3948e758980636e59d91805de776012b894b4
Ian Jackson <ijackson@clearpeople.com>
2020-03-03 18:04:01 +0000

    First commit

---
+++ app1/src/index.js
@@ -0,0 +1,8 @@
+import React from '../../app3/node_modules/react';
+import { PrimaryButton } from "../../app3/node_modules/office-ui-fabric-react";
+
+function App1() {
+  return <div style={{border: "1px solid #fff", padding: "10px"}}><PrimaryButton text="app 1"/></div>
+}
+
+export default App1
